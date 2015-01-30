8f7a1b6ea29229b8fd1fbde28cda6bdc7b3d5a39
John Reilly <johnny_reilly@hotmail.com>
2015-01-30 16:01:50 +0000

    Version driven from package.json

--- Proverb.Web/api/startup.js
+++ Proverb.Web/api/startup.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var packageJson = require('../package.json');
 
 /* GET users listing. */
 router.get('/startup',function(req, res, next) {
@@ -12,7 +13,7 @@ router.get('/startup',function(req, res, next) {
         appRoot: location + "/",
         inDebug: inDebug,
         remoteServiceRoot: location + req.baseUrl + "/",
-        version: "1.0.5506.39475" // TODO: Do we want auto incrementing version number here or is that overkill?
+        version: packageJson.version
     });
 });
 
