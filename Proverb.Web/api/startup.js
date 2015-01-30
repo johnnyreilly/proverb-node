bfa08b38d5a5a00c78b4e539488e12e73dc7540a
John Reilly <johnny_reilly@hotmail.com>
2015-01-30 11:18:45 +0000

    Tweaks

--- Proverb.Web/api/startup.js
+++ Proverb.Web/api/startup.js
@@ -4,14 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/startup',function(req, res, next) {
 
-    var location = "http://localhost:3000/"
+    var location = "//" + req.headers.host; // eg "//localhost:3000"
+    var inDebug = (express().get('env') === 'development');
 
     res.json({
-        "appName":"Proverb.Web",
-        "appRoot":location,
-        "inDebug":false,
-        "remoteServiceRoot":location + "api/",
-        "version":"1.0.5506.39475"
+        appName: "Proverb.Web",
+        appRoot: location + "/",
+        inDebug: inDebug,
+        remoteServiceRoot: location + req.baseUrl + "/",
+        version: "1.0.5506.39475" // TODO: Do we want auto incrementing version number here or is that overkill?
     });
 });
 
